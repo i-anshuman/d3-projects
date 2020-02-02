5c8e85d74c3dfd322ba4f0086402fe6265a0357b
Anshuman Gupta <anshuman844@gmail.com>
2020-02-02 21:22:15 +0530

    Initail commit after setup.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class D3Projects extends Component {
+  render () {
+    return (
+      <div className="App">
+        D3 Projects Goes here...
+      </div>
+    );
+  }
 }
 
-export default App;
+export default D3Projects;
