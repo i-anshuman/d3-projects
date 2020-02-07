abeab361e6e9dea2522cba3e2fbed74f281d3441
Anshuman Gupta <anshuman844@gmail.com>
2020-02-07 21:54:59 +0530

    Tooltip added when mouse hover over scattered circle.

--- src/scatterplot-graph/index.js
+++ src/scatterplot-graph/index.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './index.scss';
 import ScatterPlot from './plot';
-import Tooltip from '../bar-graph/tooltip';
+import Tooltip from './tooltip';
 
 class ScatterplotGraph extends Component {
   constructor (props) {
     super(props);
+    this.state = {};
     this.handleDescription = this.handleDescription.bind(this);
   }
 
@@ -16,7 +17,18 @@ class ScatterplotGraph extends Component {
   }
 
   render () {
-    console.log(this.state);
+    let tooltip = null;
+    if (this.state.desc) {
+      tooltip = <Tooltip
+        name={this.state.desc['name']}
+        year={this.state.desc['year']}
+        time={this.state.desc['time']}
+        nationality={this.state.desc['nationality']}
+        allegation={this.state.desc['allegation']}
+        x={this.state.desc.coords['x']}
+        y={this.state.desc.coords['y']}
+      />
+    }
     return (
       <div className="container">
         <div className="wrapper">
@@ -24,6 +36,7 @@ class ScatterplotGraph extends Component {
             <h1 id="title">Doping in Professional Bicycle Racing</h1>
             <h3>35 Fastest times up Alpe d'Huez</h3>
           </div>
+          {tooltip}
           <ScatterPlot width={850} height={500} desc={this.handleDescription}/>
         </div>
       </div>
