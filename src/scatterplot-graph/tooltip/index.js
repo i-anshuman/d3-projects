1b15dc9771cd1a44e528107fa3680bb67468e816
Anshuman Gupta <anshuman844@gmail.com>
2020-02-07 22:01:51 +0530

    data-date attribute changed to data-year.

--- src/scatterplot-graph/tooltip/index.js
+++ src/scatterplot-graph/tooltip/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 
 const Tooltip = (props) => {
   return (
-    <div id="tooltip" className="tooltip" data-date={props.year} style={{
+    <div id="tooltip" className="tooltip" data-year={props.year} style={{
         transform: `translateX(${props.x + 4}px) translateY(${props.y + 4}px)`
     }}>
       <span>{props.name}: {props.nationality}</span>
